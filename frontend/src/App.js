6be20aa7297fe4259069a8f30a07c326f3dbe668
MCGDev3 <elangosurya85@gmail.com>
2023-04-24 15:09:53 +0530

    Resolve Conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import { Route } from 'react-router'
-import { BrowserRouter,Routes } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
 import Login from "./login/forms/Login";
 import Register from "./login/forms/Register";
 import DashLayouts from "./dashboard/Layoutsdash";
 import Layoutsland from "./landing/Layoutsland";
+import Layoutsdemo from "./landing/Demolayout";
 import RegLayouts from './dashboard/registerationlayout';
 import RegeditLayouts from './dashboard/ClientregistrationEdit';
 import RegaddLayouts from './dashboard/ClientregistrationAdd';
@@ -25,28 +26,31 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Layoutsland/>}/>
+        <Route exact path="/" element={<Layoutsland />} />
+        <Route exact path="/Religio/Demo" element={<Layoutsdemo />} />
         <Route path='/login' element={<Login />}></Route>
         <Route path='/register' element={<Register />}></Route>
         <Route element={<PrivateRoutes />}>
-          <Route exact path="/Religio/Dashboard" element={<DashLayouts/>}></Route>
-          <Route exact path="/Religio/Dashboard" element={<DashLayouts/>}/>
-          <Route exact path="/Religio/ClientRegistration" element={<RegLayouts />}/>
-          <Route exact path="/Religio/ClientregistrationEdit/:id" element={<RegeditLayouts/>}/>
-          <Route exact path="/Religio/RegistrationAdd" element={<RegaddLayouts/>}/>
-          <Route exact path="/Religio/Congregation" element={<ConglistLayouts />}/>
-          <Route exact path="/Religio/CongregationEdit/:id" element={<CongeditLayouts />}/>
-          <Route exact path="/Religio/CongregationAdd" element={<CongaddLayouts/>}/>
-          <Route exact path="/Religio/Province" element={<ProlistLayouts />}/>
-          <Route exact path="/Religio/ProvinceEdit/:id" element={<ProeditLayouts />}/>
-          <Route exact path="/Religio/ProvinceAdd" element={<ProaddLayouts/>}/>
 
-          <Route exact path="/Religio/ProjectstatusLayouts" element={<ProjectstatusLayouts/>}/>
-          <Route exact path="/Religio/ProjectstatusAdd" element={< ProjectstatusaddLayouts/>}/>
-          <Route exact path="/Religio/ProjectstatusEdit/:id" element={< ProjectstatuseditLayouts/>}/>
-          <Route exact path="/Religio/PaymentStatus" element={<PaymentlistFile/>} />
-          <Route exact path="/Religio/PaymentCreate" element={<PaymentCreateFile/>} />
-        </Route> 
+          <Route exact path="/Religio/Dashboard" element={<DashLayouts />}></Route>
+          <Route exact path="/Religio/Dashboard" element={<DashLayouts />} />
+          <Route exact path="/Religio/ClientRegistration" element={<RegLayouts />} />
+          <Route exact path="/Religio/ClientregistrationEdit/:id" element={<RegeditLayouts />} />
+          <Route exact path="/Religio/RegistrationAdd" element={<RegaddLayouts />} />
+          <Route exact path="/Religio/Congregation" element={<ConglistLayouts />} />
+          <Route exact path="/Religio/CongregationEdit/:id" element={<CongeditLayouts />} />
+          <Route exact path="/Religio/CongregationAdd" element={<CongaddLayouts />} />
+          <Route exact path="/Religio/Province" element={<ProlistLayouts />} />
+          <Route exact path="/Religio/ProvinceEdit/:id" element={<ProeditLayouts />} />
+          <Route exact path="/Religio/ProvinceAdd" element={<ProaddLayouts />} />
+
+          <Route exact path="/Religio/ProjectstatusLayouts" element={<ProjectstatusLayouts />} />
+          <Route exact path="/Religio/ProjectstatusAdd" element={< ProjectstatusaddLayouts />} />
+          <Route exact path="/Religio/ProjectstatusEdit/:id" element={< ProjectstatuseditLayouts />} />
+          <Route exact path="/Religio/PaymentStatus" element={<PaymentlistFile />} />
+          <Route exact path="/Religio/PaymentCreate" element={<PaymentCreateFile />} />
+
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/landing/includes/Header.js
+++ frontend/src/landing/includes/Header.js
@@ -1,53 +1,59 @@
+import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-        <header>
-        {/* Header Start */}
-        <div className="header-area header-transparrent ">
-          <div className="main-header header-sticky">
-            <div className="container">
-              <div className="row align-items-center">
-                {/* Logo */}
-                <div className="col-xl-2 col-lg-2 col-md-2">
-                  <div className="logo">
-                    <a href="/Religio/Dashboard"><img src="./logo.png" alt="" /></a>
-                  </div>
+  return (
+    <header>
+      {/* Header Start */}
+      <div className="header-area header-transparrent ">
+        <div className="main-header header-sticky">
+          <div className="container">
+            <div className="row align-items-center">
+              {/* Logo */}
+              <div className="col-xl-2 col-lg-2 col-md-2">
+                <div className="logo">
+                  <a href="/Religio/Dashboard"><img src="/logo.png" alt="" /></a>
                 </div>
-                <div className="col-xl-10 col-lg-10 col-md-10">
-                  {/* Main-menu */}
-                  <div className="main-menu f-right d-none d-lg-block">
-                    <nav>
-                      <ul id="navigation">    
-                        <li><a href="#Home"> Home</a></li>
-                        {/* <li className="active"><a href="index.html"> Home</a></li> */}
-                        <li><a href="#Feature">Feature</a></li>
-                        <li><a href="#Services">Services</a></li>
-                        {/* <li><a href="#Gallery">Gallery</a></li> */}
-                        <li><a href="#Blog">Blog</a></li>
-                        {/* <li><a href="#">Pages</a>
+              </div>
+              <div className="col-xl-10 col-lg-10 col-md-10">
+                {/* Main-menu */}
+                <div className="main-menu f-right d-none d-lg-block">
+                  <nav>
+                    <ul id="navigation">
+
+                      <li><Link to="/">Home</Link></li>
+                      {/* <li className="active"><a href="index.html"> Home</a></li> */}
+                      <li><a href="#Feature">Feature</a></li>
+                      <li><a href="#Services">Services</a></li>
+                      {/* <li><a href="#Gallery">Gallery</a></li> */}
+                      <li><a href="#Blog">Blog</a></li>
+                      <li>
+                        <Link to="/Religio/Demo">Demo</Link>
+
+                      </li>
+                      {/* <li><a href="#">Pages</a>
                           <ul className="submenu">
                             <li><a href="">Blog</a></li>
                             <li><a href="">Blog Details</a></li>
                             <li><a href="">Element</a></li>
                           </ul>
                         </li> */}
-                        <li><a href="">Contact</a></li>
-                        <li><a href="/login">Login</a></li>
-                      </ul>
-                    </nav>
-                  </div>
+                      <li><a href="">Contact</a></li>
+                      <li><a href="/login">Login</a></li>
+                    </ul>
+                  </nav>
                 </div>
-                {/* Mobile Menu */}
-                <div className="col-12">
-                  <div className="mobile_menu d-block d-lg-none" />
-                </div>
+              </div>
+              {/* Mobile Menu */}
+              <div className="col-12">
+                <div className="mobile_menu d-block d-lg-none" />
               </div>
             </div>
           </div>
         </div>
-        {/* Header End */}
-      </header>
-    );
+      </div>
+      {/* Header End */}
+    </header>
+  );
 }
-  
+
 export default Header;
