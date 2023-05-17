c74338091d553d3cb9017be9559732e80a1b97eb
sakthi-sakthi <sakthiganapathis97@gmail.com>
2023-05-17 14:37:32 +0530

    implement csv export functionality
    1. Implement Export Functionality

--- backend/app/Http/Controllers/ExportController.php
+++ backend/app/Http/Controllers/ExportController.php
@@ -11,8 +11,14 @@ class ExportController extends Controller
 {
     public function exportTable()
     {
-        $records = DB::table('client_registrations')->get();
-
+  
+        $records = DB::table('client_registrations as cr')
+        ->leftjoin('congregation as co', 'co.id', 'cr.congregation')
+        ->leftjoin('provinces as pr', 'pr.id', 'cr.province')
+        ->select('co.congregation','pr.province')
+        ->select('cr.id','co.congregation','pr.province','cr.name','cr.place','cr.clienttype','cr.financialyear','cr.clientcode','cr.dateofjoining','cr.dateofcontractsigning',
+                    'cr.amcdate','cr.projectvalue','cr.amcvalue','cr.projectstatus','cr.fileattachment','cr.webapplication','cr.app','cr.website','cr.address1','cr.state','cr.address2','cr.postcode','cr.city','cr.country','cr.mobile','cr.email')
+        ->get();
         $headers = [
             'Content-Type' => 'text/csv',
             'Content-Disposition' => 'attachment; filename="client_registration.csv"',
@@ -37,7 +43,7 @@ class ExportController extends Controller
     public function exportcongregationTable()
     {
         $records = DB::table('congregation')->get();
-
+        
         $headers = [
             'Content-Type' => 'text/csv',
             'Content-Disposition' => 'attachment; filename="congregation_data.csv"',
@@ -61,7 +67,12 @@ class ExportController extends Controller
     }
     public function exportprovinceTable()
     {
-        $records = DB::table('provinces')->get();
+        $records = DB::table('client_registrations as cr')
+        ->leftjoin('congregation as co', 'co.id', 'cr.congregation')
+        ->leftjoin('provinces as pr', 'pr.id', 'cr.province')
+        ->select('pr.id','co.congregation','pr.province','pr.address1','pr.state','pr.address2','pr.postcode','pr.city','pr.country','pr.mobile','pr.email',
+        'pr.contactname','pr.contactrole','pr.contactemail','pr.contactmobile','pr.contactstatus')
+        ->get();
 
         $headers = [
             'Content-Type' => 'text/csv',
@@ -86,7 +97,22 @@ class ExportController extends Controller
     }
     public function exportpaymentTable()
     {
-        $records = DB::table('payments')->get();
+        $records = DB::table('client_registrations as cr')
+        ->leftjoin('congregation as co', 'co.id', 'cr.congregation')
+        ->leftjoin('provinces as pr', 'pr.id', 'cr.province')
+        ->leftjoin('payments as pay','cr.id','=','pay.id')
+        ->select('pay.id','co.congregation','pr.province','pay.product','pay.pi','pay.balancepaid','pay.renewelmonth','pay.gst','pay.total','pay.paid',
+                    'pay.balance','pay.clienttype','pay.financialyear','pay.clientcode','pay.projectvalue','pay.amcvalue','pay.place',    
+                    DB::raw('(CASE 
+                    WHEN pay.status IS NOT NULL THEN pay.status 
+                    ELSE "Pending"
+                    END) AS status')
+                )
+        ->get();
+
+
+    
+
 
         $headers = [
             'Content-Type' => 'text/csv',
