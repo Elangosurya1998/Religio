f1ebe3e839d19c6e02300eab3afc041ede18e98f
MCGDev3 <elangosurya85@gmail.com>
2023-05-17 09:53:50 +0530

    Update Navbar.js

--- frontend/src/dashboard/includes/Navbar.js
+++ frontend/src/dashboard/includes/Navbar.js
@@ -22,10 +22,10 @@ function Navbar() {
         </a>
       </div>
       <div className="navbar-menu-wrapper d-flex align-items-stretch">
-        {/* <button className="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+        <button className="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
           <span className="mdi mdi-menu" />
         </button>
-        <div className="search-field d-none d-md-block">
+        {/* <div className="search-field d-none d-md-block">
           <form className="d-flex align-items-center h-100" action="#">
             <div className="input-group">
               <div className="input-group-prepend bg-transparent">
@@ -138,110 +138,6 @@ function Navbar() {
               <i className="mdi mdi-format-line-spacing" />
             </a>
           </li>
-          {/* <li className="nav-item d-none d-lg-block full-screen-link">
-            <a className="nav-link">
-              <i className="mdi mdi-fullscreen" id="fullscreen-button" />
-            </a>
-          </li> 
-          <li className="nav-item dropdown">
-            <a className="nav-link count-indicator dropdown-toggle" id="messageDropdown" href="#" data-bs-toggle="dropdown" aria-expanded="false">
-              <i className="mdi mdi-email-outline" />
-              <span className="count-symbol bg-warning" />
-            </a>
-            <div className="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="messageDropdown">
-              <h6 className="p-3 mb-0">Messages</h6>
-              <div className="dropdown-divider" />
-              <a className="dropdown-item preview-item">
-                <div className="preview-thumbnail">
-                  <img src="/dashboard/assets/images/faces/face4.jpg" alt="image" className="profile-pic" />
-                </div>
-                <div className="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                  <h6 className="preview-subject ellipsis mb-1 font-weight-normal">Mark send you a message</h6>
-                  <p className="text-gray mb-0"> 1 Minutes ago </p>
-                </div>
-              </a>
-              <div className="dropdown-divider" />
-              <a className="dropdown-item preview-item">
-                <div className="preview-thumbnail">
-                  <img src="/dashboard/assets/images/faces/face2.jpg" alt="image" className="profile-pic" />
-                </div>
-                <div className="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                  <h6 className="preview-subject ellipsis mb-1 font-weight-normal">Cregh send you a message</h6>
-                  <p className="text-gray mb-0"> 15 Minutes ago </p>
-                </div>
-              </a>
-              <div className="dropdown-divider" />
-              <a className="dropdown-item preview-item">
-                <div className="preview-thumbnail">
-                  <img src="/dashboard/assets/images/faces/face3.jpg" alt="image" className="profile-pic" />
-                </div>
-                <div className="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                  <h6 className="preview-subject ellipsis mb-1 font-weight-normal">Profile picture updated</h6>
-                  <p className="text-gray mb-0"> 18 Minutes ago </p>
-                </div>
-              </a>
-              <div className="dropdown-divider" />
-              <h6 className="p-3 mb-0 text-center">4 new messages</h6>
-            </div>
-          </li>
-
-          <li className="nav-item dropdown">
-            <a className="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-bs-toggle="dropdown">
-              <i className="mdi mdi-bell-outline" />
-              <span className="count-symbol bg-danger" />
-            </a>
-            <div className="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
-              <h6 className="p-3 mb-0">Notifications</h6>
-              <div className="dropdown-divider" />
-              <a className="dropdown-item preview-item">
-                <div className="preview-thumbnail">
-                  <div className="preview-icon bg-success">
-                    <i className="mdi mdi-calendar" />
-                  </div>
-                </div>
-                <div className="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                  <h6 className="preview-subject font-weight-normal mb-1">Event today</h6>
-                  <p className="text-gray ellipsis mb-0"> Just a reminder that you have an event today </p>
-                </div>
-              </a>
-              <div className="dropdown-divider" />
-              <a className="dropdown-item preview-item">
-                <div className="preview-thumbnail">
-                  <div className="preview-icon bg-warning">
-                    <i className="mdi mdi-settings" />
-                  </div>
-                </div>
-                <div className="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                  <h6 className="preview-subject font-weight-normal mb-1">Settings</h6>
-                  <p className="text-gray ellipsis mb-0"> Update dashboard </p>
-                </div>
-              </a>
-              <div className="dropdown-divider" />
-              <a className="dropdown-item preview-item">
-                <div className="preview-thumbnail">
-                  <div className="preview-icon bg-info">
-                    <i className="mdi mdi-link-variant" />
-                  </div>
-                </div>
-                <div className="preview-item-content d-flex align-items-start flex-column justify-content-center">
-                  <h6 className="preview-subject font-weight-normal mb-1">Launch Admin</h6>
-                  <p className="text-gray ellipsis mb-0"> New admin wow! </p>
-                </div>
-              </a>
-              <div className="dropdown-divider" />
-              <h6 className="p-3 mb-0 text-center">See all notifications</h6>
-            </div>
-          </li>
-          <li className="nav-item nav-logout d-none d-lg-block">
-            <a className="nav-link" onClick={() => handleSignOut()} style={{ cursor: "pointer" }}>
-              <i className="mdi mdi-power" />
-            </a>
-          </li> 
-          <li className="nav-item nav-settings d-none d-lg-block">
-            <a className="nav-link" href="#">
-              <i className="mdi mdi-format-line-spacing" />
-            </a>
-          </li>*/}
         </ul>
         <button
           className="navbar-toggler navbar-toggler-right d-lg-none align-self-center"
